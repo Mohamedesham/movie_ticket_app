fb9546142361520edc2932b199c909c829cd0eeb
Mohamedesham <113292200+Mohamedesham@users.noreply.github.com>
2026-01-22 15:36:52 +0200

    Update API key retrieval method in api_endpoints.dart

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,6 +1,7 @@
 class ApiEndPoints {
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = String.fromEnvironment('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDcxNDI2OTI3MTc4MjkzMWVkNDJjMTBhOGNjNDNmNCIsIm5iZiI6MTc2MzEwNjIxNC45NjUsInN1YiI6IjY5MTZkZGE2ZmNhOWEyOTQyYTlhZDhjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._taSGJOxF7WATAKUMZH3XcpEWooTZ_gWJinivc4jFj4');
+  static const String apiKey = String.fromEnvironment('TMDB_API_KEY');
+
   static const String popularMovies = '/movie/popular';
   static const String movieDetails = '/movie/';
   static const String searchMovies = '/search/movie';
